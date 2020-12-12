bab8462d619815e184b4ebc674d58c91f7535562
girish kumar <girish5f9@sasi.ac.in>
2020-12-12 06:56:40 +0530

    Updates

---
+++ precache-manifest.37d2d37c77271adf50baf29379aebb56.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59c7bce48194489fd0acf4795df3e508",
+    "url": "/profileweb/index.html"
+  },
+  {
+    "revision": "589abaa8ce6bf3f9aa89",
+    "url": "/profileweb/static/css/main.e25fb0b9.chunk.css"
+  },
+  {
+    "revision": "e8fd62bd97c2b6c428ee",
+    "url": "/profileweb/static/js/2.1fb75f50.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/profileweb/static/js/2.1fb75f50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "589abaa8ce6bf3f9aa89",
+    "url": "/profileweb/static/js/main.5c5e5521.chunk.js"
+  },
+  {
+    "revision": "7f3b157efa12e4f731b4",
+    "url": "/profileweb/static/js/runtime-main.db694db8.js"
+  },
+  {
+    "revision": "48b778f8aa7dffa90d0a4af237feb40f",
+    "url": "/profileweb/static/media/bg.48b778f8.jpeg"
+  },
+  {
+    "revision": "8c1edd9afbdbc6b8a30b72bc3588d50c",
+    "url": "/profileweb/static/media/bg1.8c1edd9a.png"
+  },
+  {
+    "revision": "6083b52c455f98792736a5f702d573c6",
+    "url": "/profileweb/static/media/logo.6083b52c.png"
+  }
+]);
